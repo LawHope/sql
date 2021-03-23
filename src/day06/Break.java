99326b09ba19ade2f3d8ab1bb1a76f2fae3c8533
Law Hope . K <80802756+LawHope@users.noreply.github.com>
2021-03-24 00:45:12 +0900

    Update Break.java

--- src/day06/Break.java
+++ src/day06/Break.java
@@ -14,12 +14,12 @@ public class Break {
         Q. 1 ~ 100 총합 구하기 / 더한 값이 500을 넘는 경우 중단하기
                  */
 
-        int sum = 0;
-        int i;
+        int sum = 0; // 정수 합 = 0 이다 로 '초기화' + 선언한 거
+        int i; // i는 정수라고 선언한거
 
-        for ( i =0; i <= 100; ++i) {
-            sum += i;
-            if ( sum >= 500) break;
+        for ( i =0; i <= 100; ++i) {// i는 0부터 시작이고 100보다 작거나 같으며 1씩 증가한다.
+            sum += i; //총합은  
+            if ( sum >= 500) break; // 만약 총합이 500보다 크거나 같으면 멈춘다.
         }
         System.out.println("총합 : " + sum);
         System.out.println("중지 지점 : " + i);
