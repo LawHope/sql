e12011741643fca2ddd2a956d5bc2710114c9f04
Law Hope . K <80802756+LawHope@users.noreply.github.com>
2021-03-27 02:04:05 +0900

    Update Stuff.java

--- src/Sel_Rumination/Stuff.java
+++ src/Sel_Rumination/Stuff.java
@@ -3,14 +3,20 @@ package Sel_Rumination;
 public class Stuff {
 
     
-    1. 캡슐화 2. 다형성
+    1. 캡슐화 2. 다형성 3. ???   다음은 뭘까아요?!
 =============================================================================================================
 
-1.    캡슐화
-        ↖는 지정자 (선언 대상)에 private 붙인 거다 별거 아님         ex)    private int profeno;
+1.    캡슐화, 은닉
+        ↖는 지정자 (선언 대상)에 private 붙인 거다 별거 아님 
+                호출이 불가해져서 private 붙은 지정자 녀석은 메소드를 써야함
+        
+        ex)    private int profeno;
+               protected String Oollala;
+               public double hagisilta;       -> 이어지지  hagisilta = 3.1425673;    
 
     
-2.   다형성
+    
+2.   다형성 - 매개변수 대통합의 장
             ⓐㅡ 본체 클래스 등장! -예시로 add를 드랍한다! hulala도 됨
                                         
                      class ㅁㅁㅁ {    ↙ ●지정자 자리에 예시로 add 넣기!●
@@ -36,14 +42,22 @@ public class Stuff {
                                                     ↙ ●지정자에 umhaha.add 로 넣기!●
                         System.out.println( ●umhaha.add● ( ㅁ , ㅁㅁ)); } <- 매개변수 별로 double ㅁ int ㅁㅁ 을 안쓰고
                                                                             ㅁ,ㅁㅁ으로만 써도 정상적으로 println이 됨!!!!
-                            그거임☞  s.웜매?! = new 웜매?! ;
-                                     s.영차;
-                                     s.어기여차;
+                            그거임☞    웜매?! s = new 웜매?!();
+                                        s.영차();
+                                        s.어기여차();
                                 
                     결론 ::: 스크롤 미아가 안되도 돼!  위아래 다니며 int냐 String이냐 찾아다니지 않아도 됨!  
                     
                     
     
-    
+ 3.   
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
 
 }
