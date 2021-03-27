88348dfa837e0a1d27fdb9ac6a465ee4534e29e9
Law Hope . K <80802756+LawHope@users.noreply.github.com>
2021-03-27 17:31:24 +0900

    private 정보를 생성자, 셋 겟으로 쓰기 실습

--- src/sungjuk/SungjikV0.java
+++ src/sungjuk/SungjikV0.java
@@ -1,80 +1,59 @@
+
 package sungjuk;
 
-public class SungjikV0 {
 
-    private String name;
-    private int kor;
-    private int eng;
-    private int mat;
-    private int tot;
-    private  double  avg;
-    private  char  grd;
+private 지정자를 쓴 정보를 set get 으로 소환해서 표기하기.
+       alt + ins 로 생성자 만들기, 셋 겟 만들기 실습
+       
+       
 
+                            public class SungjikV0 {
 
-    //합계 평균 학점은 계산 결과로 나올테니 지정 생성자에서 제외
-    public SungjikV0(String name, int kor, int eng, int mat) {
-        this.name = name;
-        this.kor = kor;
-        this.eng = eng;
-        this.mat = mat;
-    }
+                                private String name;
+                                private int kor;
+                                private int eng;
+                                private int mat;
+                                private int tot;
+                                private  double  avg;
+                                private  char  grd;
 
 
+                                //합계 평균 학점은 계산 결과로 나올테니 지정 생성자에서 제외
+                                public SungjikV0(String name, int kor, int eng, int mat) {
+                                    this.name = name;
+                                    this.kor = kor;
+                                    this.eng = eng;
+                                    this.mat = mat;
+                                }
 
-    //셋 겟에선 전부다 호출 해야할테니 전부 포함해서 생성
-    public String getName() {
-        return name;
-    }
 
-    public void setName(String name) {
-        this.name = name;
-    }
 
-    public int getKor() {
-        return kor;
-    }
+                                //셋 겟에선 전부다 호출 해야할테니 전부 포함해서 생성
+                                public String getName() { }
 
-    public void setKor(int kor) {
-        this.kor = kor;
-    }
+                                public void setName(String name) { }
 
-    public int getEng() {
-        return eng;
-    }
+                                public int getKor() {}
 
-    public void setEng(int eng) {
-        this.eng = eng;
-    }
+                                public void setKor(int kor) {  }
 
-    public int getMat() {
-        return mat;
-    }
+                                public int getEng() {  }
 
-    public void setMat(int mat) {
-        this.mat = mat;
-    }
+                                public void setEng(int eng) { }
 
-    public int getTot() {
-        return tot;
-    }
+                                public int getMat() {}
 
-    public void setTot(int tot) {
-        this.tot = tot;
-    }
+                                public void setMat(int mat) {}
 
-    public double getAvg() {
-        return avg;
-    }
+                                public int getTot() { }
 
-    public void setAvg(double avg) {
-        this.avg = avg;
-    }
+                                public void setTot(int tot) {}
 
-    public char getGrd() {
-        return grd;
-    }
+                                public double getAvg() { }
 
-    public void setGrd(char grd) {
-        this.grd = grd;
-    }
+                                public void setAvg(double avg) {}
+
+                                public char getGrd() {}
+
+                                public void setGrd(char grd) {}
 }
