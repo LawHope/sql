a0777a56a8b8da5deb9bb26fedea9a250d9e5ef6
Law Hope . K <80802756+LawHope@users.noreply.github.com>
2021-03-27 17:13:21 +0900

    랜던 뽑기, Math.randome(); 사용

--- src/day06/ChooseLunch.java
+++ src/day06/ChooseLunch.java
@@ -6,23 +6,26 @@ public class ChooseLunch {
     public  static void main (String [] args) {
 
         //랜덤 머신 V.2
-        //배열로 랜덤 만들기
+        
+배열로 랜덤 만들기
+    
+    
+    
 
         String[] menu = {"김밥","버거", "돈까스", "우동","비빔면"};
 
-       // Random rnd = new Random();
-        //int  idx = rnd.nextInt(4);
-                     //난수 생성, 메뉴는 5개이므로 int 변수 범위는 0~4이어야함
+               // Random rnd = new Random();
+                //int  idx = rnd.nextInt(4);
+                             //난수 생성, 메뉴는 5개이므로 int 변수 범위는 0~4이어야함
 
-        //System.out.println(menu[idx]);
-                    //랜덤으로 메뉴를 출력
+                //System.out.println(menu[idx]);
+                            //랜덤으로 메뉴를 출력
 
 
-        double magic = Math.random(); //Math.random();으로 실제 난수가 생기는 원리
+        double magic = Math.random();  //Math.random();으로 실제 난수가 생기는 원리
         System.out.println(magic); //엄청 간단해짐
 
 
-/*
         System.out.println(magic * 10);
         System.out.println((int)(magic*10));
         //0~9사이의 랜덤숫자
@@ -40,7 +43,7 @@ public class ChooseLunch {
         //1 이상 ~ 45미만 사이 난수
         System.out.println(magic * (45 + 1 -1) + 1);
 
-*/
+
         //랜덤 메뉴 출력 
         int idx = (int) ( Math.random() * 7);
         System.out.println(menu[idx]);
