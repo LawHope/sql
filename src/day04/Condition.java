6f9382a0fe44b85fa7e484ca9d2e2f2d78102485
Law Hope . K <80802756+LawHope@users.noreply.github.com>
2021-03-27 16:25:43 +0900

    조건문 if , 세금 세율 계산기

--- src/day04/Condition.java
+++ src/day04/Condition.java
@@ -4,22 +4,31 @@ import java.util.Scanner;
 
 public class Condition {
 public static void main (String [] args) {
-	 
-//조건문
-//원래는 순차적으로 진행하는 Sequence지만 특정 코드만 실행해야할 때
-		//if, switch
 	
-//if
-		//조건식 결과가 참 혹은 거짓일 때 각각 실행함
-		//if ( 조건식 ) { 참일 때 실행부 } else { 거짓일 때 실행부 }
-		//else가 분기점이네
+	
+	
+	
+	
+	 
+조건문	Condition
+
+	
+		원래는 순차적으로 진행하는 Sequence지만 특정 코드만 실행해야할 때
+		if, switch
+			
+	
+if
+		조건식 결과가 참 혹은 거짓일 때 각각 실행함
+		if ( 조건식 ) { 참일 때 실행부 } else { 거짓일 때 실행부 }
+		else가 분기점이네
+
 		
-		//1-100 짝수 출력하기
+					 수열지옥;;
+		1-100 짝수 출력하기
 		for(int i = 1; i<=100; ++i) { //참인 부분 시작
 			if(i % 2 == 0 ) //i가 짝수라면 2로 나눈 나머지가 0일테니까
 				System.out.print(i + ", "); }// 실행
 
-System.out.println();
 
 		for(int i = 0; i<=100; ++i) {
 			if(i % 2 ==0)
@@ -27,15 +36,17 @@ System.out.println();
 			else
 				 System.out.println(i + "는 홀수입니다.");
 		}
+	
 
-System.out.println();		
-
+	
+	
 		//연봉 salary 과 결혼 married 여부를 입력하면 
 		//세율과 세금을 계산
 		//미혼 : 0 ; 연봉 5000미만 - 10%, 5000이상 - 25%
 		//기혼 : 1 ; 연봉 5000미만 - 15%, 5000이상 - 35%
 
 		//미혼 기혼 / 미만 이상
+	
 		int mar;	
 		int sal;	
 		double tax;
@@ -64,11 +75,7 @@ System.out.println();
 				
 				
 				
-				
-			
-		
-//switch =>월요일에!!
-				
+
 	 	
 	 	
 	 	
