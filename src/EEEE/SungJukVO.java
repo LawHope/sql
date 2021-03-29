aa6526d6d6283e18a3a14cf580db10e390110501
LawHope <lawhope.biz@gmail.com>
2021-03-29 23:28:43 +0900

    singletone / static class / Up & Down Casting

--- src/EEEE/SungJukVO.java
+++ src/EEEE/SungJukVO.java
@@ -1,27 +1,22 @@
-package sungjuk;
+package EEEE;
 
-public class SungjikV0 {
+public class SungJukVO {
 
-    private String name;
-    private int kor;
-    private int eng;
-    private int mat;
-    private int tot;
-    private  double  avg;
-    private  char  grd;
+    protected String name;
+    protected int kor;
+    protected int eng;
+    protected int mat;
+    protected int tot;
+    protected double avg;
+    protected char grd;
 
-
-    //합계 평균 학점은 계산 결과로 나올테니 지정 생성자에서 제외
-    public SungjikV0(String name, int kor, int eng, int mat) {
+    public SungJukVO(String name, int kor, int eng, int mat) {
         this.name = name;
         this.kor = kor;
         this.eng = eng;
         this.mat = mat;
     }
 
-
-
-    //셋 겟에선 전부다 호출 해야할테니 전부 포함해서 생성
     public String getName() {
         return name;
     }
